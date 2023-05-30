96b058a68e9942688531e801ebef68de59b7ec1d
LEKH-RAJ-AWASTHI <lekhrajattariyakailali@gmail.com>
2023-05-30 12:39:22 +0545

    self-learn

---
+++ src/app/New/New-card/patient.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'patient-diagnosis',
+  // template: `<h1> Patient Diagnosis </h1>
+  templateUrl: './patient.component.html',
+  // styles: [`h1{color:blue}`]
+  styleUrls: ['./patient.component.css']
+})
+export class PatientDiagnosisComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { PatientDiagnosisComponent } from './New/New-card/patient.component';
+import { PatientListComponent } from './patient-list/patient-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientDiagnosisComponent,
+    PatientListComponent
   ],
   imports: [
     BrowserModule,
