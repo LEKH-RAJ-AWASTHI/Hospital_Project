46b309c579f78687108505c14b3bf93f4358d6c5
LEKH-RAJ-AWASTHI <lekhrajattariyakailali@gmail.com>
2023-06-10 11:12:35 +0545

    revised code from june 10 class and added remove functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,43 @@
 import { Component } from '@angular/core';
+import { Patient } from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'HospitalProject';
-  name:string = "";
-  age:number = 0;
-  ClickMe(){
+  patient: Patient = new Patient();
+  patientArr: Array<Patient> = new Array<Patient>();
+  isEditing: boolean = false;
 
-    alert("Hello " + this.name + " you are " + this.age + " years old.");
+  editPatient(patientSelected: Patient) {
+    this.patient = new Patient();
+    this.patient.age = patientSelected.age;
+    this.patient.name = patientSelected.name; //this is decoupling
+    this.patient.id = patientSelected.id;
+  }
+  AddOrUpdatePatient() {
+    if (this.patient.id == 0) { //new patient
+      this.patient.id = this.patientArr.length + 1;
+      this.patientArr.push(this.patient);
+    }
+    else { //update existing patient
+      this.patientArr.forEach((element) => {
+        if (element.id == this.patient.id) {
+          element.name = this.patient.name;
+          element.age = this.patient.age;
+        }
+
+      });
+    }
+    this.patient = new Patient();
+  }
+  deletePatient(patientSelected: Patient) {
+    this.patientArr.forEach((element, index) => {
+      if (element.id == patientSelected.id) {
+        this.patientArr.splice(index, 1);
+      }
+    });
   }
 }
--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -1,6 +1,6 @@
 export class Patient
 {
-
+  id:number=0;
   name:string="";
   age:number=0;
 }
